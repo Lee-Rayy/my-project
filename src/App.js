b319b51f2406512890fb277c190ab70914ad2204
Lee-Rayy <lee_storey@icloud.com>
2024-12-19 19:14:06 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import HelloWorld from "./components/HelloWorld";
+
+import './App.css';
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <HelloWorld/>
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HelloWorld() {
+  return (
+    <div></div>
+  )
+}
+
+export default HelloWorld
